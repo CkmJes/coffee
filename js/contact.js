62cd923c0799d2e13dd31fc167f0a06382b38c8e
CkmJes <ckm.jesu8426@gmail.com>
2025-09-03 02:16:43 -0700

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -15,3 +15,41 @@ fetch('pages/contact.html')
   .catch(error => {
     console.error('Error loading choose.html:', error);
   });
+
+
+// emailjs.init("kowsalya0724@gmail.com"); 
+//   document.getElementById("supportForm").addEventListener("submit", function(e) {
+//   e.preventDefault();
+//   let form = e.target;
+//   fetch(form.action, {
+//     method: form.method,
+//     body: new FormData(form),
+//     headers: { 'Accept': 'kowsalya0724@gmail.com' }
+//   }).then(response => {
+//     if (response.ok) {
+//       document.getElementById("formMessage").textContent = "Message sent successfully!";
+//       form.reset();
+//     } else {
+//       document.getElementById("formMessage").textContent = "Oops! Something went wrong.";
+//     }
+//   });
+// });
+
+
+
+document.getElementById("supportForm").addEventListener("submit", function(e) {
+  e.preventDefault(); // prevent form from submitting
+  const form = e.target;
+
+  // check if form is valid
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+
+  // Show success message
+  document.getElementById("formMessage").textContent = "Message sent successfully!";
+  
+  // Reset form
+  form.reset();
+});
